c88096d8ee41f82fed100bb601409a7790d804e5
Nandini Jain <113116163+Nandini26-prog@users.noreply.github.com>
2024-01-23 01:07:23 +0530

    Time: 4 ms (67.72%) | Memory: 44.8 MB (7.06%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sch=s.toCharArray();
+        char[] tch=t.toCharArray();
+
+        Arrays.sort(sch);
+        Arrays.sort(tch);
+
+        return Arrays.equals(sch,tch);
+    }
+    }
