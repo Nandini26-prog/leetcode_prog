a522f1f5b0489cca944c407c8d0d9fc50bd9a6de
Nandini Jain <113116163+Nandini26-prog@users.noreply.github.com>
2024-01-24 01:05:11 +0530

    Time: 4 ms (16.55%) | Memory: 45.9 MB (51.66%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int n=nums.length;
+         List<Integer> temp=new ArrayList<Integer>();
+         for(int i=0;i<n;i++)  {
+             if(nums[i]!=0){
+                 temp.add(nums[i]);
+             }
+         }  
+         int m=temp.size();  
+         for(int i=0;i<m;i++){
+             nums[i]=temp.get(i);
+         }
+         for(int i=m;i<n;i++)
+            nums[i]=0;
+    }
+}
