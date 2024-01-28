76a54c1db49c9b104ba93747aef3185bfc1dbf0d
Nandini Jain <113116163+Nandini26-prog@users.noreply.github.com>
2024-01-29 00:08:16 +0530

    Time: 3 ms (84.27%) | Memory: 44.8 MB (33.49%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -16,16 +16,15 @@ class Solution {
                 i++;
             }
         }
-       // if(s.length()>=2){
+      
         if(value(s.charAt(s.length()-1))<=value(s.charAt(s.length()-2)))
             sum=sum+value(s.charAt(s.length()-1));
         }
-        // else{
-        //     diff=value(s.charAt(s.length()-1))-value(s.charAt(s.length()-2));
-        //     sum=sum+diff;
-        // }
+       
         return sum;
     }
+
+
     public int value(char s){
         if(s=='I')    return 1;
         else if(s=='V')   return 5;
@@ -34,5 +33,6 @@ class Solution {
         else if(s=='C')   return 100;
         else if(s=='D')   return 500;
         else if(s=='M')   return 1000;
-    else return 0;}
+        else return 0;
+    }
 }
